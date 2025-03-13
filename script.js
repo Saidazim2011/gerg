49de4f63058491ea95c78d133b8769b159cb0f6c
Saidazim2011 <oergasev75@gmail.com>
2025-03-13 09:08:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName = prompt("Ismingizni kiriting!");
+const age = prompt("Yoshingizni kiriting!");
+const city = prompt("Manzillingizni kiriting!");
+
+
+
+
+console.log(`Salom mening ismim ${firstName} va men ${age} yoshdaman.Men ${city} danman.`);
